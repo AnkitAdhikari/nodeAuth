42c1ea8ad4b7c0410a414e66dd03e29e49172200
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-08-14 22:13:19 +0545

    prevent duplicate user regestritaion and implemented user login (Day 12)

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("You are in home page");
+  res.render("home.ejs");
 });
 // register form
 
@@ -21,13 +21,52 @@ app.get("/register", (req, res) => {
 
 // POST API (register)
 app.post("/createUser", async (req, res) => {
-  await users.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 10),
+  // const username = req.body.username;
+  const userFound = await users.findAll({
+    where: {
+      email: req.body.email,
+    },
   });
+  if (userFound.length === 0) {
+    await users.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 10),
+    });
+    res.redirect("/login");
+  } else {
+    res.send("email already registered");
+  }
   // finally create vayepaxi
-  res.send("User registered sucessfully");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post("/login", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  console.log(username, email, password);
+
+  const userFound = await users.findAll({
+    where: {
+      email,
+    },
+  });
+
+  if (userFound.length === 0) {
+    res.send("Email not regitsters");
+  } else {
+    const databasePassword = userFound[0].password;
+    const isCorrect = bcrypt.compareSync(password, databasePassword);
+
+    if (isCorrect) {
+      res.send("Login sucessful");
+    } else {
+      res.send("Invalid Email or Password");
+    }
+  }
 });
 
 // listening the server
